7df41b393f6cf93d7be0d2d56cdef645241e2338
Luc Constantin <lucc.constantin@gmail.com>
2023-03-06 12:26:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#combustible-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const distance = parseFloat(document.querySelector('#distance').value.replace(/,/g, ''));
+  const price = parseFloat(document.querySelector('#price').value.replace(/,/g, ''));
+  const consumption = parseFloat(document.querySelector('#consumption').value.replace(/,/g, ''));
+  const cost = (distance / 100) * consumption * price;
+  const resultDiv = document.querySelector('#result');
+  resultDiv.innerHTML = `Result: The total cost of combustible used for ${distance.toLocaleString()} kilometers is ${cost.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} Euros (€).`;
+});
+
+function CalculateNew() {
+    // var msg;
+    // msg= " Recalculate?";
+    // var agree=confirm(msg);
+    // if (agree)
+    return window.location.reload();
+    // else
+    // return false;
+    }
