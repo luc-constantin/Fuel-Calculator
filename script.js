db60df072194b2c9b94cf789d0c819f450456425
Luc Constantin <lucc.constantin@gmail.com>
2023-03-09 12:56:38 +0100

    added new reset Button

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const form = document.querySelector('#combustible-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const distance = parseFloat(document.querySelector('#distance').value.replace(/,/g, ''));
-  const price = parseFloat(document.querySelector('#price').value.replace(/,/g, ''));
-  const consumption = parseFloat(document.querySelector('#consumption').value.replace(/,/g, ''));
-  const cost = (distance / 100) * consumption * price;
-  const resultDiv = document.querySelector('#result');
-  resultDiv.innerHTML = `Result: The total cost of combustible used for ${distance.toLocaleString()} kilometers is ${cost.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} Euros (€).`;
-});
+const distanceInput = document.getElementById('distance');
+	const priceInput = document.getElementById('price');
+	const consumptionInput = document.getElementById('consumption');
+	const calculateButton = document.getElementById('calculate');
+	const resultDiv = document.getElementById('result');
+	const resetButton = document.getElementById('reset');
 
-function CalculateNew() {
-  //uncomment the lines: 14, 15, 16, 17, 19, 20 to display a message when the NEW SUMS button is clicked
-    // var msg;
-    // msg= " Recalculate?";
-    // var agree=confirm(msg);
-    // if (agree)
-    return window.location.reload();
-    // else
-    // return false;
-    }
+	// Enable calculate button when all fields are filled
+	function checkFields() {
+	  if (distanceInput.value && priceInput.value && consumptionInput.value) {
+		calculateButton.disabled = false;
+		calculateButton.style.backgroundColor = "#9966ff";
+	  } else {
+		calculateButton.disabled = true;
+		calculateButton.style.backgroundColor = "#ccb3ff";
+		calculateButton.style.color ="#FFF"
+	  }
+	}
+
+	// Calculate fuel consumption and display result
+	function calculate() {
+	  const distance = distanceInput.value;
+	  const price = priceInput.value;
+	  const consumption = consumptionInput.value;
+	  const fuel = distance / 100 * consumption;
+	  const cost = fuel * price;
+	  resultDiv.innerHTML = `You have consumed ${fuel.toFixed(2)} liters of fuel, which cost €${cost.toFixed(2)}.`;
+	  resetButton.style.display = "block";
+	}
+
+	// Reset form and hide reset button
+	function reset() {
+	  document.getElementById("distance").value = "";
+	  document.getElementById("price").value = "";
+	  document.getElementById("consumption").value = "";
+	  resultDiv.innerHTML = "";
+	  resetButton.style.display = "none";
+	  calculateButton.disabled = true;
+	  calculateButton.style.backgroundColor = "";
+	}
+
+	distanceInput.addEventListener('input', checkFields);
+	priceInput.addEventListener('input', checkFields);
+	consumptionInput.addEventListener('input', checkFields);
+	calculateButton.addEventListener('click', calculate);
+	resetButton.addEventListener('click', reset);
+
