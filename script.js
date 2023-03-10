2c9b30bb2126d2afa30b0500cd67522fe9985729
Luc Constantin <lucc.constantin@gmail.com>
2023-03-10 10:28:55 +0100

    Update the JavaScript code to V 1.0.2
     Update the JavaScript code to V 1.0.2 and make it cleaner.

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 const distanceInput = document.getElementById('distance');
-	const priceInput = document.getElementById('price');
-	const consumptionInput = document.getElementById('consumption');
-	const calculateButton = document.getElementById('calculate');
-	const resultDiv = document.getElementById('result');
-	const resetButton = document.getElementById('reset');
+const priceInput = document.getElementById('price');
+const consumptionInput = document.getElementById('consumption');
+const calculateButton = document.getElementById('calculate');
+const resultDiv = document.getElementById('result');
+const resetButton = document.getElementById('reset');
 
-	// Enable calculate button when all fields are filled
-	function checkFields() {
-	  if (distanceInput.value && priceInput.value && consumptionInput.value) {
-		calculateButton.disabled = false;
-		calculateButton.style.backgroundColor = "#9966ff";
-	  } else {
-		calculateButton.disabled = true;
-		calculateButton.style.backgroundColor = "#ccb3ff";
-		calculateButton.style.color ="#FFF"
-	  }
-	}
+// Enable calculate button when all fields are filled
+function checkFields() {
+  if (distanceInput.value && priceInput.value && consumptionInput.value) {
+	calculateButton.disabled = false;
+	calculateButton.style.backgroundColor = "#9966ff";
+  } else {
+	calculateButton.disabled = true;
+	calculateButton.style.backgroundColor = "#ccb3ff";
+	calculateButton.style.color ="#000"
+  }
+}
 
-	// Calculate fuel consumption and display result
-	function calculate() {
-	  const distance = distanceInput.value;
-	  const price = priceInput.value;
-	  const consumption = consumptionInput.value;
-	  const fuel = distance / 100 * consumption;
-	  const cost = fuel * price;
-	  resultDiv.innerHTML = `You have consumed ${fuel.toFixed(2)} liters of fuel, which cost €${cost.toFixed(2)}.`;
-	  resetButton.style.display = "block";
-	}
+// Calculate fuel consumption and display result
+function calculate() {
+  const distance = distanceInput.value;
+  const price = priceInput.value;
+  const consumption = consumptionInput.value;
+  const fuel = distance / 100 * consumption;
+  const cost = fuel * price;
+  resultDiv.innerHTML = `You have consumed ${fuel.toFixed(2)} liters of fuel, which cost €${cost.toFixed(2)}.`;
+  resetButton.style.display = "block";
+}
 
-	// Reset form and hide reset button
-	function reset() {
-	  document.getElementById("distance").value = "";
-	  document.getElementById("price").value = "";
-	  document.getElementById("consumption").value = "";
-	  resultDiv.innerHTML = "";
-	  resetButton.style.display = "none";
-	  calculateButton.disabled = true;
-	  calculateButton.style.backgroundColor = "";
-	}
+// Reset form and hide reset button
+function reset() {
+  document.getElementById("distance").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("consumption").value = "";
+  resultDiv.innerHTML = "";
+  resetButton.style.display = "none";
+  calculateButton.disabled = true;
+  calculateButton.style.backgroundColor = "";
+}
 
-	distanceInput.addEventListener('input', checkFields);
-	priceInput.addEventListener('input', checkFields);
-	consumptionInput.addEventListener('input', checkFields);
-	calculateButton.addEventListener('click', calculate);
-	resetButton.addEventListener('click', reset);
+distanceInput.addEventListener('input', checkFields);
+priceInput.addEventListener('input', checkFields);
+consumptionInput.addEventListener('input', checkFields);
+calculateButton.addEventListener('click', calculate);
+resetButton.addEventListener('click', reset);
+
+
 
